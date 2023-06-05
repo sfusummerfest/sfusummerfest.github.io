0cd04863d196b43b861b8236ba99aef56938003f
quachtridat <quachtridat@yahoo.com.vn>
2023-06-04 21:21:08 -0700

    Bootstrap the website using `create-next-app`
    - Bootstrap the website using `npx create-next-app`.
    - Strip off boilerplate from the template given by `create-next-app`.
    - Add a GitHub Actions workflow `build-deploy.yml` to build and deploy
      the website to GitHub Pages.

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the website of SFU Summer Festival.
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="relative flex-col place-items-center">
+        <h2 className="mb-3 text-2xl font-semibold">MAINTENANCE MODE</h2>
+      </div>
+    </main>
+  )
+}
