2810947fdef6426c56eda11800f9213d2bb08422
quachtridat <quachtridat@yahoo.com.vn>
2023-06-13 02:22:32 -0700

    Add header section for homepage
    This replaces the current "MAINTENANCE MODE" page with a page consisting
    of only the header section.
    
    The newly-implemented section is a background video behind a darkened
    overlay. On top of it, there are the SFU Summer Festival logo and the
    "Enter" button.
    
    This part is done desktop-first. It works best on a 1920x1080px screen.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
+import "@fortawesome/fontawesome-free/js/all.min";
+import Image from "next/image";
+import Link from "next/link";
+
+import logoImage from "@/public/logos/the-summer-festival.png"
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative flex-col place-items-center">
-        <h2 className="mb-3 text-2xl font-semibold">MAINTENANCE MODE</h2>
-      </div>
-    </main>
+    <div className="flex min-h-screen flex-col items-center justify-between space-y-8">
+      <header className="relative h-screen w-full">
+        <div className="absolute top-0 left-0 w-full h-full" id="home-bg-video-container">
+          <iframe width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/3FAz7KMoKrA?autoplay=1&controls=0&rel=0&playsinline=1&enablejsapi=1&loop=1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        </div>
+        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-70"></div>
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center">
+          <div className="max-w-4xl">
+            <Image src={logoImage} alt="Logo of The Summer Festival presented by SFU Anime" unoptimized></Image>
+          </div>
+          <Link href="#home-main"><button className="border-white border-4 rounded-full py-4 px-8 bg-[#DB796D] hover:bg-[#FFA266] focus:bg-[#FFA266] active:bg-[#FFA266] text-white font-bold">Enter</button></Link>
+        </div>
+      </header>
+    </div>
   )
 }
