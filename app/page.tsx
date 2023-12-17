27ddb37ce777e944ca5853769c2fd20cfeaf47af
Tri Dat Quach <quachtridat@yahoo.com.vn>
2023-12-17 05:31:13 -0800

    Keep the max line length at 80
    This change adjusts `app/page.tsx` so that the max line length is 80 for
    every line, and also reorders attributes alphabetically for all HTML
    elements.

--- app/page.tsx
+++ app/page.tsx
@@ -6,25 +6,61 @@ import logoImage from "@/public/logos/the-summer-festival.png"
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-between min-h-screen space-y-8">
+    <div
+      className={`
+        flex flex-col items-center justify-between min-h-screen space-y-8
+      `}
+    >
       <header className="h-screen relative w-full">
-        <div className="absolute h-full left-0 top-0 w-full" id="home-bg-video-container">
-          <iframe width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/3FAz7KMoKrA?autoplay=1&controls=0&rel=0&playsinline=1&enablejsapi=1&loop=1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        <div
+          className="absolute h-full left-0 top-0 w-full"
+          id="home-bg-video-container"
+        >
+          <iframe
+            allow={`
+              accelerometer; autoplay; clipboard-write; encrypted-media;
+              gyroscope; picture-in-picture; web-share
+            `}
+            allowFullScreen
+            height="100%"
+            src="https://www.youtube.com/embed/3FAz7KMoKrA?autoplay=1"
+            title="YouTube video player"
+            width="100%"
+          ></iframe>
         </div>
-        <div className="absolute bg-black h-full left-0 opacity-70 top-0 w-full"></div>
-        <div className="absolute flex flex-col h-full items-center justify-center left-0 top-0 w-full">
+        <div
+          className="absolute bg-black h-full left-0 opacity-70 top-0 w-full"
+        ></div>
+        <div
+          className={`
+            absolute flex flex-col h-full items-center justify-center left-0
+            top-0 w-full
+          `}
+        >
           <div className="max-w-4xl">
-            <Image src={logoImage} alt="Logo of The Summer Festival presented by SFU Anime" unoptimized></Image>
+            <Image
+              alt="Logo of The Summer Festival presented by SFU Anime"
+              src={logoImage}
+              unoptimized
+            ></Image>
           </div>
           <Link href="#home-main">
-            <button className="active:bg-primary-purple active:border-primary-yellow active:text-primary-yellow bg-primary-red border-4 border-white font-bold hocus:bg-primary-purple hocus:border-primary-yellow hocus:text-primary-yellow px-8 py-4 rounded-full text-white">
-              Enter
-            </button>
+            <button
+              className={`
+                active:bg-primary-purple active:border-primary-yellow
+                active:text-primary-yellow bg-primary-red border-4
+                border-white font-bold hocus:bg-primary-purple
+                hocus:border-primary-yellow hocus:text-primary-yellow px-8
+                py-4 rounded-full text-white
+              `}
+            >Enter</button>
           </Link>
         </div>
       </header>
       <main className="max-w-screen-lg px-2 space-y-4" id="home-main">
-        <section className="border-4 border-primary-red p-2 rounded-xl space-y-4">
+        <section
+          className="border-4 border-primary-red p-2 rounded-xl space-y-4"
+        >
           <div>
             <h2 className="font-bold text-3xl text-primary-red">
               <i className="fa-circle-info fa-solid"></i>&nbsp;
@@ -34,30 +70,47 @@ export default function Home() {
           </div>
           <div className="leading-normal space-y-2 text-lg">
             <p>
-              Hello everyone, we, unfortunately, have some difficult news to share with you all.
+              Hello everyone, we, unfortunately, have some difficult news to
+              share with you all.
             </p>
             <p>
-              Due to challenges presented with preparations and regulatory requirements, we will <b>not</b> be hosting the SFU Summer Festival for 2023.
-              We understand that this is something that many regular attendees have been looking forward to, it is a long-standing tradition for us as a club and it hurts us to be put into a situation where we cannot foresee running the event this year.
+              Due to challenges presented with preparations and regulatory
+              requirements, we will <b>not</b> be hosting the SFU Summer
+              Festival for 2023. We understand that this is something that many
+              regular attendees have been looking forward to, it is a long-
+              standing tradition for us as a club and it hurts us to be put into
+              a situation where we cannot foresee running the event this year.
             </p>
             <p>
-              However, this is not the end for SummerFest, even though we do not plan to run the event this year, we are looking to ensure that it will happen next year for 2024.
-              Many of our concerns for this year&apos;s SummerFest lie in newfound limitations, accessibility, and complications in ensuring a safe venue space.
-              We plan to take a year to reevaluate our event plan, layout, and location and anticipate significant changes in the coming festivals.
+              However, this is not the end for SummerFest, even though we do not
+              plan to run the event this year, we are looking to ensure that it
+              will happen next year for 2024. Many of our concerns for this
+              year&apos;s SummerFest lie in newfound limitations, accessibility,
+              and complications in ensuring a safe venue space. We plan to take
+              a year to reevaluate our event plan, layout, and location and
+              anticipate significant changes in the coming festivals.
             </p>
             <p>
-              A main concern for us is that we do not wish to bring something that would not meet the same standards as in previous years&apos; festivals.
-              Last year saw a fantastic turnout and it welcomed many new people to the landscape, and we wish to ensure that we can keep striving for something more, to keep growing, instead of taking a step back.
+              A main concern for us is that we do not wish to bring something
+              that would not meet the same standards as in previous years&apos;
+              festivals. Last year saw a fantastic turnout and it welcomed many
+              new people to the landscape, and we wish to ensure that we can
+              keep striving for something more, to keep growing, instead of
+              taking a step back.
             </p>
             <p>
-              We from the SFU Summer Festival Committee sincerely apologize to all of you who were looking forward to this year&apos;s event, and we thank you all for your understanding of this situation.
+              We from the SFU Summer Festival Committee sincerely apologize to
+              all of you who were looking forward to this year&apos;s event, and
+              we thank you all for your understanding of this situation.
             </p>
             <p className="text-right">
               - SFU Summer Festival Planning Committee
             </p>
           </div>
         </section>
-        <section className="border-4 border-primary-red p-2 rounded-xl space-y-4">
+        <section
+          className="border-4 border-primary-red p-2 rounded-xl space-y-4"
+        >
           <div>
             <h2 className="font-bold text-3xl text-primary-red">
               <i className="fa-brands fa-square-youtube"></i>&nbsp;
@@ -66,13 +119,32 @@ export default function Home() {
             <hr />
           </div>
           <div className="h-full w-full">
-            <iframe width="100%" height="576px" src="https://www.youtube.com/embed/vAZB4xjWcdc" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+            <iframe
+              allow={`
+                accelerometer; autoplay; clipboard-write; encrypted-media;
+                gyroscope; picture-in-picture; web-share
+              `}
+              allowFullScreen
+              height="576px"
+              src="https://www.youtube.com/embed/vAZB4xjWcdc"
+              title="YouTube video player"
+              width="100%"
+            ></iframe>
           </div>
         </section>
       </main>
-      <footer className="bg-primary-dark flex flex-col items-center justify-between space-y-8 w-full">
+      <footer
+        className={`
+          bg-primary-dark flex flex-col items-center justify-between space-y-8
+          w-full
+        `}
+      >
         <section className="max-w-xs">
-          <Image src={logoImage} alt="Logo of The Summer Festival presented by SFU Anime" unoptimized></Image>
+          <Image
+            alt="Logo of The Summer Festival presented by SFU Anime"
+            src={logoImage}
+            unoptimized
+          ></Image>
         </section>
         <section className="space-x-2 text-white text-xl">
           <Link href="https://www.facebook.com/sfusummerfest">
@@ -107,7 +179,9 @@ export default function Home() {
           </Link>
         </section>
         <section>
-          <p className="text-gray-500 text-xs">&copy; SFU Summer Festival 2022-2023</p>
+          <p className="text-gray-500 text-xs">
+            &copy; SFU Summer Festival 2022-2023
+          </p>
         </section>
       </footer>
     </div>
