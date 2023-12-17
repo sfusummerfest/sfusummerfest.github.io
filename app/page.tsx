31c8d269ef07e391d6ade41bb10800dc29cfd6b6
Tri Dat Quach <quachtridat@yahoo.com.vn>
2023-12-17 05:08:25 -0800

    Move arbitrary color codes to TailwindCSS config
    This commit moves arbitrary color codes in `app/page.tsx` to
    `tailwind.config.js`, and additionally adds a custom TailwindCSS variant
    `hocus` to shorten code that applies the same color for both the hover
    and the focus states of an element.
    
    This commit also:
    - Sorts all class names alphabetically in `app/page.tsx`.
    - Moves the constant `576px` from the 2022 VOD's `div`'s classes to the
      `height` property of the `iframe` in the said `div`.

--- app/page.tsx
+++ app/page.tsx
@@ -6,34 +6,38 @@ import logoImage from "@/public/logos/the-summer-festival.png"
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between space-y-8">
-      <header className="relative h-screen w-full">
-        <div className="absolute top-0 left-0 w-full h-full" id="home-bg-video-container">
+    <div className="flex flex-col items-center justify-between min-h-screen space-y-8">
+      <header className="h-screen relative w-full">
+        <div className="absolute h-full left-0 top-0 w-full" id="home-bg-video-container">
           <iframe width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/3FAz7KMoKrA?autoplay=1&controls=0&rel=0&playsinline=1&enablejsapi=1&loop=1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         </div>
-        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-70"></div>
-        <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center">
+        <div className="absolute bg-black h-full left-0 opacity-70 top-0 w-full"></div>
+        <div className="absolute flex flex-col h-full items-center justify-center left-0 top-0 w-full">
           <div className="max-w-4xl">
             <Image src={logoImage} alt="Logo of The Summer Festival presented by SFU Anime" unoptimized></Image>
           </div>
-          <Link href="#home-main"><button className="border-white border-4 rounded-full py-4 px-8 bg-[#DB796D] hover:bg-[#FFA266] focus:bg-[#FFA266] active:bg-[#FFA266] text-white font-bold">Enter</button></Link>
+          <Link href="#home-main">
+            <button className="active:bg-primary-purple active:border-primary-yellow active:text-primary-yellow bg-primary-red border-4 border-white font-bold hocus:bg-primary-purple hocus:border-primary-yellow hocus:text-primary-yellow px-8 py-4 rounded-full text-white">
+              Enter
+            </button>
+          </Link>
         </div>
       </header>
       <main className="max-w-screen-lg px-2 space-y-4" id="home-main">
-        <section className="p-2 text-[#DB796D] border-[#DB796D] border-4 rounded-xl space-y-4">
+        <section className="border-4 border-primary-red p-2 rounded-xl space-y-4">
           <div>
-            <h2 className="text-3xl font-bold">
-              <i className="fa-solid fa-circle-info"></i>&nbsp;
+            <h2 className="font-bold text-3xl text-primary-red">
+              <i className="fa-circle-info fa-solid"></i>&nbsp;
               Important information regarding SFU Summer Festival 2023
             </h2>
-            <hr/>
+            <hr />
           </div>
-          <div className="text-lg leading-normal space-y-2">
+          <div className="leading-normal space-y-2 text-lg">
             <p>
               Hello everyone, we, unfortunately, have some difficult news to share with you all.
             </p>
             <p>
-              Due to challenges presented with preparations and regulatory requirements, we will not be hosting the SFU Summer Festival for 2023.
+              Due to challenges presented with preparations and regulatory requirements, we will <b>not</b> be hosting the SFU Summer Festival for 2023.
               We understand that this is something that many regular attendees have been looking forward to, it is a long-standing tradition for us as a club and it hurts us to be put into a situation where we cannot foresee running the event this year.
             </p>
             <p>
@@ -48,62 +52,62 @@ export default function Home() {
             <p>
               We from the SFU Summer Festival Committee sincerely apologize to all of you who were looking forward to this year&apos;s event, and we thank you all for your understanding of this situation.
             </p>
-            <p>
+            <p className="text-right">
               - SFU Summer Festival Planning Committee
             </p>
           </div>
         </section>
-        <section className="p-2 text-[#DB796D] border-[#DB796D] border-4 rounded-xl space-y-4">
+        <section className="border-4 border-primary-red p-2 rounded-xl space-y-4">
           <div>
-            <h2 className="text-3xl font-bold">
+            <h2 className="font-bold text-3xl text-primary-red">
               <i className="fa-brands fa-square-youtube"></i>&nbsp;
               SFU Summer Festival 2022 VOD
             </h2>
-            <hr/>
+            <hr />
           </div>
-          <div className="w-full h-[576px]">
-            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/vAZB4xjWcdc" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+          <div className="h-full w-full">
+            <iframe width="100%" height="576px" src="https://www.youtube.com/embed/vAZB4xjWcdc" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
           </div>
         </section>
       </main>
-      <footer className="w-full bg-[#351211] flex flex-col items-center justify-between space-y-8">
+      <footer className="bg-primary-dark flex flex-col items-center justify-between space-y-8 w-full">
         <section className="max-w-xs">
           <Image src={logoImage} alt="Logo of The Summer Festival presented by SFU Anime" unoptimized></Image>
         </section>
-        <section className="text-white text-xl space-x-2">
+        <section className="space-x-2 text-white text-xl">
           <Link href="https://www.facebook.com/sfusummerfest">
-            <span className="p-3 bg-[#3B5998] rounded-md">
+            <span className="bg-brand-facebook p-3 rounded-md">
               <i className="fa-brands fa-facebook"></i>
             </span>
           </Link>
           <Link href="https://www.twitter.com/SFUSummerFest">
-            <span className="p-3 bg-[#1DA1F2] rounded-md">
+            <span className="bg-brand-twitter p-3 rounded-md">
               <i className="fa-brands fa-twitter"></i>
             </span>
           </Link>
           <Link href="https://www.instagram.com/sfusummerfest">
-            <span className="p-3 bg-[#262626] rounded-md">
+            <span className="bg-brand-instagram p-3 rounded-md">
               <i className="fa-brands fa-instagram"></i>
             </span>
           </Link>
           <Link href="https://www.twitch.tv/sfusummerfest">
-            <span className="p-3 bg-[#6441A5] rounded-md">
+            <span className="bg-brand-twitch p-3 rounded-md">
               <i className="fa-brands fa-twitch"></i>
             </span>
           </Link>
           <Link href="https://www.youtube.com/channel/UCioH5wqn7VSG0valSMrD5VA">
-            <span className="p-3 bg-[#CD201F] rounded-md">
+            <span className="bg-brand-youtube p-3 rounded-md">
               <i className="fa-brands fa-youtube"></i>
             </span>
           </Link>
           <Link href="https://www.discord.gg/sfuanime">
-            <span className="p-3 bg-[#7289DA] rounded-md">
+            <span className="bg-brand-discord p-3 rounded-md">
               <i className="fa-brands fa-discord"></i>
             </span>
           </Link>
         </section>
         <section>
-          <p className="text-xs text-gray-500">&copy; SFU Summer Festival 2022-2023</p>
+          <p className="text-gray-500 text-xs">&copy; SFU Summer Festival 2022-2023</p>
         </section>
       </footer>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand-discord': '#7289DA',
+        'brand-facebook': '#3B5998',
+        'brand-instagram': '#262626',
+        'brand-twitch': '#6441A5',
+        'brand-twitter': '#1DA1F2',
+        'brand-youtube': '#FF0000',
+        'primary-dark': '#351211',
+        'primary-orange': '#FBB767',
+        'primary-purple': '#724E91',
+        'primary-red': '#F48779',
+        'primary-yellow': '#FFD151',
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant('hocus', ['&:hover', '&:focus'])
+    }),
   ],
 }
