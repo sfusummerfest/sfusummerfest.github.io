f21d15dd86b4dfd723b8b0486fe59ea472f0e50a
Tri Dat Quach <quachtridat@yahoo.com.vn>
2023-12-18 03:23:32 -0800

    Modify `next.config.js` for image export optimizer
    This commit sets the image loader to `custom` and add
    `next-image-export-optimizer` to the list of transpile packages.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+  images: {
+    loader: 'custom',
+  },
+  transpilePackages: [
+    'next-image-export-optimizer'
+  ],
+  output: 'export',
 }
 
 module.exports = nextConfig
