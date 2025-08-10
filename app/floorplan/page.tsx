db1569268465f69cc27b7b6bc5d89b514e709d9b
Brian <le.brian125@gmail.com>
2025-08-10 14:55:40 -0700

    Update sponsors, floorplan

--- app/floorplan/page.tsx
+++ app/floorplan/page.tsx
@@ -1,8 +1,8 @@
-import floorplan2024 from '@/public/images/2024_SummerFest_Public_Floorplan.webp';
+import floorplan2025 from '@/public/images/2025_SummerFest_Public_Floorplan.webp';
 
 export const metadata = {
     title: 'Summerfest Floorplan',
-    description: 'View the Summerfest 2024 floorplan.',
+    description: 'View the Summerfest 2025 floorplan.',
 };
 
 export default function Floorplan() {
@@ -10,22 +10,22 @@ export default function Floorplan() {
         <article className='md:max-w-screen-2xl mx-auto px-4 pt-16 pb-20'>
             <header className="border-b-2 border-stone-950 sm:text-center my-10 pb-8">
                 <h2 className="text-4xl font-bold text-stone-950">
-                    Summerfest 2024 Floorplan
+                    Summerfest 2025 Floorplan
                 </h2>
                 <p className="text-lg text-stone-700">
-                    3700 Willingdon Ave SE16, Burnaby, BC V5G 3H2
+                    8888 University Drive, Burnaby, BC V5A 1S6
                 </p>
             </header>
             <div className="relative w-full flex flex-col items-center">
                 <div className="w-full aspect-[16/9] bg-gray-200">
                     <img
-                        src={floorplan2024.src}
-                        alt="Summerfest 2024 Floorplan"
+                        src={floorplan2025.src}
+                        alt="Summerfest 2025 Floorplan"
                         className="w-full h-full object-contain"
                     />
                 </div>
                 <a
-                    href={floorplan2024.src}
+                    href={floorplan2025.src}
                     target="_blank"
                     rel="noopener noreferrer"
                     className="mt-4 text-primary-red hover:underline text-base lg:text-lg"
--- components/Sponsors.js
+++ components/Sponsors.js
@@ -1,5 +1,8 @@
 import Image from 'next/image';
+import resist from '@/public/images/sponsors/Logo_Thick_Large_X_RED_v02.png';
+import fantuan from '@/public/images/sponsors/Fantuan.png'
 import hobbyBee from '@/public/images/sponsors/hobby-bee-logo.webp';
+import huion from '@/public/images/sponsors/Huion_current_logo_vertical_arrangement.svg.png'
 import konbiniya from '@/public/images/sponsors/konbiniya.webp';
 import mouseCollectibles from '@/public/images/sponsors/mouse-collectibles.webp';
 import nimbasa from '@/public/images/sponsors/Nimbasa.webp';
@@ -8,14 +11,26 @@ import TokyoTreatSvg from '@/components/TokyoTreatSvg';
 import plamod from '@/public/images/sponsors/plamod.webp';
 import sfss from '@/public/images/sponsors/SFSS_COLOR_LOGO.webp';
 import studioSIAT from '@/public/images/sponsors/SFU_SIAT_logo.png';
+import sunsetStudios from '@/public/images/sponsors/Sunsetvisitor_Logo_Horizontal_Black.webp'
+import treehouseToys from '@/public/images/sponsors/New-Toys-logo-1_03.png'
+import turtleKeebs from '@/public/images/sponsors/Turtle_Keebs.png'
+import yakg from '@/public/images/sponsors/YAKG.png';
 
 const sponsors = [
+    { src: fantuan, alt: 'Fantuan', link: 'https://fantuan.ca/en/'},
     { src: hobbyBee, alt: 'Hobby Bee Canada', link: 'https://hobby-bee.com/' },
+    { src: huion, alt: 'Huion', link: 'https://store.huion.com/ca/?gad_source=1&gad_campaignid=21263945663&gbraid=0AAAAAqns4SYxQbJGVMY5vJmiKV-KOfd6e&gclid=CjwKCAjwwNbEBhBpEiwAFYLtGOKR3y28gwkhIEtoMWqG3-W353IWYljZe5Jo5z8Om-kVSVFRaSrLBhoCZ1MQAvD_BwE'},
     { src: mouseCollectibles, alt: 'Mouse Collectibles' },
     { src: nimbasa, alt: 'Nimbasa Entertainment', link: 'https://nimbasa.co/' },
+    { src: resist, alt: '1000xResist', link: 'https://store.steampowered.com/app/1675830/1000xRESIST/'},
     { src: sakuraMedia, alt: 'Sakura Media', link: 'https://www.instagram.com/sakuramediametro/' },
     { src: studioSIAT, alt: 'StudioSIAT', link: 'http://www.sfu.ca/siat/studiosiat.html'},
-    { src: sfss, alt: 'Simon Fraser Student Society', link: 'https://sfss.ca/'}
+    { src: sunsetStudios, alt: 'Sunset Studios', link: 'https://www.sunsetvisitor.studio/'},
+    { src: sfss, alt: 'Simon Fraser Student Society', link: 'https://sfss.ca/'},
+    { src: treehouseToys, alt: 'Treehouse Toys', link: 'https://treehousetoys.ca/'},
+    { src: turtleKeebs, alt: 'Turtle Keebs', link: 'https://turtlekeebs.com/?srsltid=AfmBOoptKw6BsY3o1RStQCzDC9bi3WqShBkoK62NeppXeDxiLEtyZOIA'},
+    { component: TokyoTreatSvg, isSvg: true, alt: 'Tokyo Treat', link: 'https://tokyotreat.com/?srsltid=AfmBOooVLxp5ogXvBqVv777hN3fs1GBAZ_RGshxwpAJIhv6glK4TMVWD'},
+    { src: yakg, alt: 'Yet Another Killing Game', link: 'https://linktr.ee/yakg'},
     // { src: plamod, alt: 'Plamod Distributors Inc', link: 'https://www.instagram.com/plamod_official/' },
 
 ];
